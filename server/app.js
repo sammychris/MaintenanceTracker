49e71dfaf76829c5b7ac3181955cd35dd53a6a3a
Samuel Christopher <Ebusameric@gmail.com>
2018-08-07 20:04:44 +0100

    required the route

--- server/app.js
+++ server/app.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import logger from 'morgan';
+import route from './routes';
 
 
 const app = express();
 
 app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+route(app)
+
 
 export default app;
