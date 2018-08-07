d854e2016cdd165544e830006ef8a447819cb9c6
Samuel Christopher <Ebusameric@gmail.com>
2018-08-07 09:54:26 +0100

    create db and users

---
+++ server/models/user.js
@@ -0,0 +1,21 @@
+
+const db = [
+				{
+					firstname:'samuel',
+					lastname: 'christopher',
+					email   : 'ebusameric@gmail.com',
+					password: 'ebusam12'
+				},
+				{
+					firstname:'john',
+					lastname: 'emeka',
+					email   : 'emaka@gmail.com',
+					password: 'emeka12345'
+				},
+				{
+					firstname:'samson',
+					lastname: 'johnson',
+					email   : 'samson@gmail.com',
+					password: 'johnson12345'
+				}
+];
