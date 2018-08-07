e13766252f80d85708d072a7ac6790d7583f6573
Samuel Christopher <Ebusameric@gmail.com>
2018-08-07 20:59:11 +0100

    create route for create request and view all request

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,10 @@
 import user from '../controllers/user';
+import request from '../controllers/request';
 
 export default (app) => {
 	app.post('/auth/signup', user.signUp);
 	app.post('/auth/login', user.logIn);
+
+	app.get('/users/requests', request.allReq);
+	app.post('/users/requests', request.create);
 }
