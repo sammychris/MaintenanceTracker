fbf1b53beef1e67d35af09f02ea4713c86d7bea3
Samuel Christopher <Ebusameric@gmail.com>
2018-08-07 20:07:11 +0100

    to validate input fields

--- server/models/validator.js
+++ server/models/validator.js
@@ -1,26 +1,28 @@
 
-export default  {
-	error : false,
-	message : '',
+// export default  {
+// 	error : false,
+// 	message : '',
 
-	type(data,type){
-		type = type.toLowerCase();
-		if(typeof data == type){
-			error = true;
-			this.message = `${data} Invalid value type. `;
-		}
-	},
+// 	type(data,type){
+// 		type = type.toLowerCase();
+// 		if(typeof data !== type){
+// 			this.error = true;
+// 			this.message = `Invalid value type. `;
+// 		}
+// 	},
 
-	len(data, leng){
-		if (data.length >= leng){
-			this.error = true;
-			this.message += `length should not be less than ${leng}`;
-		}
-	},
+// 	len(data, leng){
+// 		if (data.length <= leng){
+// 			this.error = true;
+// 			this.message += `Length should not be less than ${leng}. `;
+// 		}
+// 	},
 
-	isMail(data){
-		if(/^[a-z\d]+@{1}[a-z\d]+.com$/.test(data)){
-			this.error = true;
-			this.message += `email not in the right Format`;
-		}
-}
+// 	isMail(data){
+// 		if(!/^[a-z\d]+@{1}[a-z\d]+.com$/.test(data)){
+// 			this.error = true;
+// 			this.message += `Email not in the right Format`;
+// 		}
+// 	}
+// }
+
