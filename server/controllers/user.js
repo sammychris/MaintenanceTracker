21c89bd74233e7f13e5e59cf9dae370b0f535518
Samuel Christopher <Ebusameric@gmail.com>
2018-08-07 20:08:06 +0100

    create the signup and login controllers

---
+++ server/controllers/user.js
@@ -0,0 +1,25 @@
+import user from '../models/userSchema';
+import request from '../models/requestSchema';
+
+
+export default {
+	signUp(req, res){
+		const { firstname, lastname, email, password }  = req.body;
+		const info = {
+					firstname: firstname,
+					lastname: lastname,
+					email   : email,
+					password: password,
+					request : []
+			}
+		user.db.push(info)
+		res.json(info);
+	},
+	logIn(req, res){
+		const { email, password} = req.body;
+		let findUser = user.db.find(email);
+		if(!findUser) return res.send('user does not exist!');
+		if(findUser.password !== password) return res.send('wrong password');
+		res.send('user logged in successful!');
+	}
+}
