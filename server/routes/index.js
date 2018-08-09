523064888f9d4329298ce588887047f65869d0ab
Samuel Christopher <Ebusameric@gmail.com>
2018-08-09 14:36:27 +0100

    allByUser route created

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,12 +5,12 @@ export default (app) => {
 	app.post('/auth/signup', user.signUp);
 	app.post('/auth/login', user.logIn);
 
-	app.get('/users/requests', request.allReq);
+	app.get('/users/requests', request.allByUser);
 	app.get('/users/requests/:requestId', request.aReq);
 	app.post('/users/requests', request.create);
 	app.put('/users/requests/:requestId', request.modify);
 
-	app.get('/requests/', request.all);
+	app.get('/requests/', request.allReq);
 	app.put('/requests/:requestId/approve', request.approve);
 	app.put('/requests/:requestId/disapprove', request.disapprove);
 	app.put('/requests/:requestId/resolve', request.resolve);
