1686f4fd26b77befb4684b7c452d8e8d9a2628ee
Samuel Christopher <Ebusameric@gmail.com>
2018-08-17 20:34:58 +0100

    added cors

--- server/app.js
+++ server/app.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import logger from 'morgan';
+import cors from 'cors';
 import route from './routes';
 
 
 const app = express();
-
 app.use(logger('dev'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(`${__dirname}/../UI`));
+app.use(cors());
+
 route(app);
 
+app.get('/*', (req, res) => res.send('welcome'));
 export default app;
