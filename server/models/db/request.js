07b81d235716b44adb2d65106f150d00313a9cb2
Samuel Christopher <Ebusameric@gmail.com>
2018-08-17 11:11:07 +0100

    added more data

--- server/models/db/request.js
+++ server/models/db/request.js
@@ -2,8 +2,36 @@ export default [
     {
         name: 'Mark johnson',
         date: '23/2/2018',
-        type: 'repair',
-        status: 'pending',
+        type: 'Repair',
+        status: 'Maintenance',
+        description: 'I want to run some maintenance no my car.',
+    },
+    {
+        name: 'James johnson',
+        date: '03/4/2018',
+        type: 'Repair',
+        status: 'Maintenance',
+        description: 'I want to run some maintenance no my car.',
+    },
+    {
+        name: 'Patric Usman',
+        date: '24/6/2018',
+        type: 'Repair',
+        status: 'Pending',
+        description: 'I want to run some maintenance no my car.',
+    },
+    {
+        name: 'Emeka Emmanuel',
+        date: '23/2/2018',
+        type: 'Repair',
+        status: 'Pending',
+        description: 'I want to run some repair no my car.',
+    },
+    {
+        name: 'Philip Samuel',
+        date: '09/5/2018',
+        type: 'Maintenance',
+        status: 'Pending',
         description: 'I want to run some maintenance no my car.',
     },
 ];
