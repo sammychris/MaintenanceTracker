03c53b12b9d0200562e0081c50a8e066c4855a6d
Samuel Christopher <Ebusameric@gmail.com>
2018-08-19 03:35:14 +0100

    add verification for all route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,22 @@
+import { config } from 'dotenv';
 import user from '../controllers/user';
 import request from '../controllers/request';
+import auth from '../middleware/authorize';
+
+config(); //  configuring my environmental variable
+const { verifyUser, verifyAdmin } = auth;
 
 export default (app) => {
     app.post('/auth/signup', user.signUp);
     app.post('/auth/login', user.logIn);
 
-    app.get('/users/requests', request.allByUser);
-    app.get('/users/requests/:requestId', request.aReq);
-    app.post('/users/requests', request.create);
-    app.put('/users/requests/:requestId', request.modify);
+    app.get('/users/requests', request.allByUser, verifyUser);
+    app.get('/users/requests/:requestId', request.aReq, verifyUser);
+    app.post('/users/requests', request.create, verifyUser);
+    app.put('/users/requests/:requestId', request.modify, verifyUser);
 
-    app.get('/requests', request.allReq);
-    app.put('/requests/:requestId/approve', request.approve);
-    app.put('/requests/:requestId/disapprove', request.disapprove);
-    app.put('/requests/:requestId/resolve', request.resolve);
+    app.get('/requests', request.allReq, verifyAdmin);
+    app.put('/requests/:requestId/approve', request.approve, verifyAdmin);
+    app.put('/requests/:requestId/disapprove', request.disapprove, verifyAdmin);
+    app.put('/requests/:requestId/resolve', request.resolve, verifyAdmin);
 };
