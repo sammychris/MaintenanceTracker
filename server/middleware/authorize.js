aed589e92101eef99a06199bbda28bf69b6d36aa
Samuel Christopher <Ebusameric@gmail.com>
2018-08-19 03:26:35 +0100

    add user and admin verification

---
+++ server/middleware/authorize.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+
+
+export default {
+
+    // middleware to verification token for users
+    verifyUser(req, res, next) {
+        const { headers } = req;
+        const token = headers.authorization || headers['x-access-token'] || req.body.token;
+        if (token) {
+            jwt.verify(token, process.env.USER_KEY, (err) => {
+                if (err) return res.status(403).json('Token Is Not Valid');
+                return next();
+            });
+        } else {
+            res.status(401).json('Token not provided');
+        }
+    },
+
+    // middleware to verification token for Admin
+    verifyAdmin(req, res, next) {
+        const { headers } = req;
+        const token = headers.authorization || headers['x-access-token'] || req.body.token;
+        if (token) {
+            jwt.verify(token, process.env.ADMIN_KEY, (err) => {
+                if (err) return res.status(403).json({ message: 'Token Is Not Valid' });
+                return next();
+            });
+        } else {
+            res.status(401).json('Token not provided');
+        }
+    },
+
+};
