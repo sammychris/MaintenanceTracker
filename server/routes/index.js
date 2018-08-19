3e0ec8eb47878718ed969973885a3ec5927e0aea
Samuel Christopher <Ebusameric@gmail.com>
2018-08-19 03:43:46 +0100

    modify route

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,13 +10,13 @@ export default (app) => {
     app.post('/auth/signup', user.signUp);
     app.post('/auth/login', user.logIn);
 
-    app.get('/users/requests', request.allByUser, verifyUser);
-    app.get('/users/requests/:requestId', request.aReq, verifyUser);
-    app.post('/users/requests', request.create, verifyUser);
-    app.put('/users/requests/:requestId', request.modify, verifyUser);
+    app.get('/users/requests', verifyUser, request.allByUser);
+    app.get('/users/requests/:requestId', verifyUser, request.aReq);
+    app.post('/users/requests', verifyUser, request.create);
+    app.put('/users/requests/:requestId', verifyUser, request.modify);
 
-    app.get('/requests', request.allReq, verifyAdmin);
-    app.put('/requests/:requestId/approve', request.approve, verifyAdmin);
-    app.put('/requests/:requestId/disapprove', request.disapprove, verifyAdmin);
-    app.put('/requests/:requestId/resolve', request.resolve, verifyAdmin);
+    app.get('/requests', verifyAdmin, request.allReq);
+    app.put('/requests/:requestId/approve', verifyAdmin, request.approve);
+    app.put('/requests/:requestId/disapprove', verifyAdmin, request.disapprove);
+    app.put('/requests/:requestId/resolve', verifyAdmin, request.resolve);
 };
