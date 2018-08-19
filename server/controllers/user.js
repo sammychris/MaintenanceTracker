bd7811c175a6a891762dbf4721f9cb1b9bdf2019
Samuel Christopher <Ebusameric@gmail.com>
2018-08-19 03:13:33 +0100

    added jwt to create user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken';
 import user from '../models/userSchema';
 
 
@@ -14,11 +15,15 @@ export default {
             password,
             request: [],
         };
-        user.db.push(info);
-        return res.json(info);
+        jwt.sign(info, process.env.USER_KEY, { expiresIn: '1h' }, (err, token) => {
+            if (err) return console.log(err);
+            user.db.push(info);
+            return res.status(201).json({ user: 'successfully registered!', token });
+        });
     },
 
     logIn(req, res) {
+        const adminOrUser = (req.body.admin) ? process.env.ADMIN_KEY : process.env.USER_KEY;
         const { email, password } = req.body;
         const findUser = user.db.find(v => v.email === email.toLowerCase());
         if (!findUser) return res.send('user does not exist!');
