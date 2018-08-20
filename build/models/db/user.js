3bf227fd9e9184ed78934e6dfe99e5cc5f6d01f1
Samuel Christopher <Ebusameric@gmail.com>
2018-08-20 18:47:22 +0100

    files for production env

---
+++ build/models/db/user.js
@@ -0,0 +1,54 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = [{
+    firstname: 'samuel',
+    lastname: 'christopher',
+    email: 'ebusameric@gmail.com',
+    password: 'ebusam12',
+    role: ['admin']
+}, {
+    firstname: 'Emeka',
+    lastname: 'Emmanuel',
+    email: 'emaka@gmail.com',
+    password: '11111',
+    request: [{
+        name: 'Emeka Emmanuel',
+        date: '23/2/2018',
+        type: 'Repair',
+        status: 'Pending',
+        description: 'I want to run some maintenance no my car.'
+    }, {
+        name: 'Emeka Emmanuel',
+        date: '03/4/2018',
+        type: 'Maintenance',
+        status: 'Accepted',
+        description: 'I want to run some maintenance no my car.'
+    }]
+}, {
+    firstname: 'Patric',
+    lastname: 'Usman',
+    email: 'patric@gmail.com',
+    password: '11111',
+    request: [{
+        name: 'Patric Usman',
+        date: '24/6/2018',
+        type: 'Repair',
+        status: 'Pending',
+        description: 'I want to run some maintenance no my car.'
+    }, {
+        name: 'Patric Usman',
+        date: '23/2/2018',
+        type: 'Repair',
+        status: 'Pending',
+        description: 'I want to run some repair no my car.'
+    }, {
+        name: 'Patric Usman',
+        date: '09/5/2018',
+        type: 'Maintenance',
+        status: 'Pending',
+        description: 'I want to run some maintenance no my car.'
+    }]
+}];
