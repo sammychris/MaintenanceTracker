9d57b61ce4ade9fc4502720ebc4a9cd1cde86c38
Samuel Christopher <Ebusameric@gmail.com>
2018-08-20 16:02:17 +0100

    change response message

--- server/middleware/authorize.js
+++ server/middleware/authorize.js
@@ -9,11 +9,11 @@ export default {
         const token = headers.authorization || headers['x-access-token'] || req.body.token;
         if (token) {
             jwt.verify(token, process.env.USER_KEY, (err) => {
-                if (err) return res.status(403).json({ message: 'Token Is Not Valid' });
+                if (err) return res.status(403).json({ error: 'Invalid Token' });
                 return next();
             });
         } else {
-            res.status(401).json('Token not provided');
+            res.status(401).json({ error: 'Token not provided' });
         }
     },
 
@@ -23,11 +23,11 @@ export default {
         const token = headers.authorization || headers['x-access-token'] || req.body.token;
         if (token) {
             jwt.verify(token, process.env.ADMIN_KEY, (err) => {
-                if (err) return res.status(403).json({ message: 'Token Is Not Valid' });
+                if (err) return res.status(403).json({ error: 'Invalid Token' });
                 return next();
             });
         } else {
-            res.status(401).json({ message: 'Token not provided' });
+            res.status(401).json({ error: 'Token not provided' });
         }
     },
 
