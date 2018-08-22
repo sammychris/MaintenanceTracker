ba1e10f40a9ad347b0fcda673f87d654da6799e8
Samuel Christopher <Ebusameric@gmail.com>
2018-08-22 19:38:17 +0100

    component created for admin dashboard

--- client/admin-component/admin.js
+++ client/admin-component/admin.js
@@ -1,3 +1,7 @@
+import '../stylesheet/style.css';
+import '../stylesheet/w3.css';
+import '../fonts';
+
 const logOut = document.getElementById('log-out');
 
 // if it eventually falls in this page? go back to user.
