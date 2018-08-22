d73efbc0956b3d23c856f42ffcc1643f0e1e4c24
Samuel Christopher <Ebusameric@gmail.com>
2018-08-22 12:45:18 +0100

    adding more configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,60 @@
-const path = require('path');
+/* eslint-disable */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+const outputDirectory = "dist";
+
+
+function htmlFile( name, sourceName = './UI/index.html' ) {
+	console.log(sourceName);
+	return new HtmlWebpackPlugin({ filename : name, template: sourceName});
+}
+
+
+//htmlFile({filename: "contents/users.html", template: "./UI/contents/users.html" });
 
 module.exports = {
-    entry: './UI/js/app.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+  entry: "./UI/js/app.js",
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader?limit=100000"
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    proxy: {
+      "/api": "http://localhost:8080"
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin([outputDirectory]),
+    htmlFile('index'),
+    htmlFile('contents/user','./UI/contents/users.html'),
+    htmlFile('contents/user-requests','./UI/contents/users.html'),
+    htmlFile('contents/sign-up','./UI/contents/users.html'),
+    htmlFile('contents/modify-request','./UI/contents/users.html'),
+    htmlFile('admin/index','./UI/admin/index.html'),
+    htmlFile('admin/admin-requests','./UI/contents/users.html'),
+    htmlFile('admin/admin-dashboard','./UI/contents/users.html')
+  ]
 };
