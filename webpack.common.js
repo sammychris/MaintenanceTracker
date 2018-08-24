c778c0358b49db4c55cfaaf8dcca169fdb2334f4
Samuel Christopher <Ebusameric@gmail.com>
2018-08-24 13:15:03 +0100

    webpack common configuration

---
+++ webpack.common.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+const path = require("path");
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const UgligyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const outputDirectory = "dist";
+const generateHTML = ( name, sourceName ) => new HtmlWebpackPlugin({ filename : name, template: sourceName });
+
+
+
+
+module.exports = {
+  entry: "./client/app.js",
+   plugins: [
+    new CleanWebpackPlugin([outputDirectory]),
+    // User
+    new HtmlWebpackPlugin({template: './UI/index.html'}),
+    generateHTML('contents/modify-request.html', './UI/contents/modify-request.html'),
+    generateHTML('contents/sign-up.html', './UI/contents/sign-up.html'),
+    generateHTML('contents/users-requests.html', './UI/contents/users-requests.html'),
+    generateHTML('contents/users.html', './UI/contents/users.html'),
+    // Admin
+    generateHTML('admin/dashboard.html', './UI/admin/dashboard.html'),
+    generateHTML('admin/requests.html', './UI/admin/requests.html'),
+    generateHTML('admin/index.html', './UI/admin/index.html'),
+  ],
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: "bundle.js"
+  }
+};
