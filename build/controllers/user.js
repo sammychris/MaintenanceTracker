b8df493c0c1d469a39d60cde619067abbeb0f8fa
Samuel Christopher <Ebusameric@gmail.com>
2018-08-24 15:15:36 +0100

    add dotenv

--- build/controllers/user.js
+++ build/controllers/user.js
@@ -8,11 +8,17 @@ var _jsonwebtoken = require('jsonwebtoken');
 
 var _jsonwebtoken2 = _interopRequireDefault(_jsonwebtoken);
 
+var _dotenv = require('dotenv');
+
+var _dotenv2 = _interopRequireDefault(_dotenv);
+
 var _userSchema = require('../models/userSchema');
 
 var _userSchema2 = _interopRequireDefault(_userSchema);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_dotenv2.default.config();
 
 exports.default = {
     signUp: function signUp(req, res) {
