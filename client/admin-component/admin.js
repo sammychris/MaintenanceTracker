3b907defdcca0764aa215a7c95322ae7996bc8d5
Samuel Christopher <Ebusameric@gmail.com>
2018-08-24 13:34:31 +0100

    remove import style sheet and location.assign url change

--- client/admin-component/admin.js
+++ client/admin-component/admin.js
@@ -1,12 +1,8 @@
-import '../stylesheet/style.css';
-import '../stylesheet/w3.css';
-import '../fonts';
-
 const logOut = document.getElementById('log-out');
 
 // if it eventually falls in this page? go back to user.
 if (localStorage.getItem('userToken')) {
-    location.assign('../../contents/users');
+    location.assign('../contents/users.html');
 }
 
 
