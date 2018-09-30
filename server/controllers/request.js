0d873c2bd854c7fd0ce4dce4544908aca927318d
Samuel Christopher <Ebusameric@gmail.com>
2018-09-30 07:39:42 +0100

    restructure code to find request by id

--- server/controllers/request.js
+++ server/controllers/request.js
@@ -1,7 +1,11 @@
+import SSEChannel from 'sse-pubsub';
 import request from '../models/requestSchema';
 import user from '../models/userSchema';
 
 let updated;
+const channel = new SSEChannel();
+
+
 export default {
     create(req, res) {
         const { type, description } = req.body;
@@ -9,25 +13,30 @@ export default {
         const currentDate = (date.getDate() < 10) ? `0${date.getDate()}` : date.getDate();
         const fullDate = ` ${currentDate}/${date.getMonth()}/${date.getFullYear()} `;
         const userReq = {
+            id: `${request.db.length}`,
             date: fullDate,
             type,
             status: 'pending',
             description,
+            sorting: Date.now(),
         };
         const { id } = req.headers;
-        const userById = user.db[id];
+        const userById = user.db.find(a => a.id === id);
         if (!userById) return res.status(404).send({ error: 'User does not exist' });
         userReq.name = `${userById.firstname} ${userById.lastname}`;
         request.db.push(userReq);
-        userById.request.push(userReq);
+        userById.requestId.push(userReq.id);
+        userReq.userPic = userById.profilePicSrc;
+        channel.publish(userReq, 'myEvent'); // notifying admin...
         return res.json({ message: 'Your request has been sent' });
     },
 
     allByUser(req, res) {
         const { id } = req.headers;
-        const userById = user.db[id];
+        const userById = user.db.find(a => a.id === id);
         if (!userById) return res.status(404).send({ error: 'user does not exist!' });
-        if (userById.request.length < 1) return res.status(400).send({ message: 'This user has made no request yet!' });
+        if (userById.requestId.length < 1) return res.status(400).send({ message: 'This user has made no request yet!' });
+        userById.request = request.db.filter(a => userById.requestId.includes(a.id));
         return res.json({ requests: userById.request });
     },
 
@@ -37,9 +46,7 @@ export default {
 
     aReq(req, res) {
         const { requestId } = req.params;
-        const { id } = req.headers;
-        const userById = user.db[id];
-        const findReq = userById.request[requestId];
+        const findReq = request.db.find(a => a.id === requestId);
         if (!findReq) return res.status(401).send({ error: '404 request id does not exists!' });
         return res.json({ request: findReq });
     },
@@ -47,9 +54,7 @@ export default {
     modify(req, res) {
         if (updated) return res.status(400).send({ message: 'Oops! you have to wait for five minute before you can modify' });
         const { requestId } = req.params;
-        const { id } = req.headers;
-        const userById = user.db[id];
-        const findReq = userById.request[requestId];
+        const findReq = request.db.find(a => a.id === requestId);
         if (!findReq) return res.status(404).send({ error: '404 request id does not exists!' });
         findReq.type = req.body.type;
         findReq.description = req.body.description;
@@ -60,28 +65,32 @@ export default {
 
     approve(req, res) {
         const { requestId } = req.params;
-        const findReq = request.db[requestId];
+        const findReq = request.db.find(a => a.id === requestId);
         if (!findReq) return res.status(404).send({ error: '404 request id does not exists!' });
-        if (findReq.status !== 'pending') return res.status(400).send({ error: 'request must be pending before approval!' });
-        findReq.status = 'approved';
-        return res.send({ approved: findReq });
+        if (findReq.status.toLowerCase() !== 'pending') return res.status(400).send({ error: 'request must be pending before approval!' });
+        findReq.status = 'accepted';
+        return res.send({ accepted: findReq });
     },
 
     disapprove(req, res) {
         const { requestId } = req.params;
-        const findReq = request.db[requestId];
+        const findReq = request.db.find(a => a.id === requestId);
         if (!findReq) return res.status(404).send({ error: '404 request id does not exists!' });
-        if (findReq.status !== 'pending') return res.status(400).send({ error: 'request must be pending before disapproved!' });
-        findReq.status = 'disapproved';
-        return res.send({ disapproved: findReq });
+        if (findReq.status.toLowerCase() !== 'pending') return res.status(400).send({ error: 'request must be pending before disapproved!' });
+        findReq.status = 'rejected';
+        return res.send({ rejected: findReq });
     },
 
     resolve(req, res) {
         const { requestId } = req.params;
-        const findReq = request.db[requestId];
+        const findReq = request.db.find(a => a.id === requestId);
         if (!findReq) return res.status(404).send({ error: '404 request id does not exists!' });
-        if (findReq.status !== 'approved') return res.status(400).send({ error: 'request must be approved before resolve!' });
+        if (findReq.status.toLowerCase() !== 'accepted') return res.status(400).send({ error: 'request must be approved before resolve!' });
         findReq.status = 'resolved';
         return res.send({ resolved: findReq });
+    },
+
+    notification(req, res) {
+        return channel.subscribe(req, res);
     },
 };
