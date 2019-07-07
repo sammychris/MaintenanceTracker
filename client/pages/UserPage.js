eca8c3686acbf25a7c5943a3caa29f67d07de604
Samuel Christopher <Ebusameric@gmail.com>
2019-07-07 09:44:44 +0100

    userpage functionality

--- client/pages/UserPage.js
+++ client/pages/UserPage.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { GetAllRequests, LogOut } from './services';
 
+class UserPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {},
+      requests: [],
+      login: '',
+    };
+  }
 
-const UserPage = () => {
-  return (
-    <div>
-      <h1> this is the user page</h1>
-    </div>
-  );
-};
+  componentDidMount() {
+    this.setState({
+      user: JSON.parse(localStorage.getItem('user')),
+    });
+    GetAllRequests('/user/requests').then(reqs => console.log(reqs));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome come mr {this.state.user.name}</h1>
+        <Link to="/"><button onClick={LogOut}>logout</button></Link>
+      </div>
+    );
+  }
+}
 
 
 export default UserPage;
