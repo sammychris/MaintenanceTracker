e2bdacbda70b34f26bf1d2af29ba250fd251b512
Samuel Christopher <Ebusameric@gmail.com>
2019-07-07 09:46:03 +0100

    header authorization

---
+++ client/pages/helpers/index.js
@@ -0,0 +1,10 @@
+
+export default function authHeader() {
+  // return authorization header with basic auth credentials
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    return { authorization: token };
+  }
+  return {};
+}
