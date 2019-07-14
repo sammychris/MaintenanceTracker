b6d746aa8aa78396f2200c8574a3026c8330906b
Samuel Christopher <Ebusameric@gmail.com>
2019-07-14 07:14:55 +0100

    build the request for userpage

--- client/pages/UserPage.js
+++ client/pages/UserPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { GetAllRequests, LogOut } from './services';
+import { getAllRequests, logOut } from './services';
+import { Header, AsideNav, RequestList } from './components';
+
 
 class UserPage extends React.Component {
   constructor(props) {
@@ -8,7 +10,6 @@ class UserPage extends React.Component {
     this.state = {
       user: {},
       requests: [],
-      login: '',
     };
   }
 
@@ -16,14 +17,52 @@ class UserPage extends React.Component {
     this.setState({
       user: JSON.parse(localStorage.getItem('user')),
     });
-    GetAllRequests('/user/requests').then(reqs => console.log(reqs));
+    getAllRequests('/user/requests').then((reqs) => {
+      console.log(reqs);
+      this.setState({ requests: reqs });
+    });
   }
 
   render() {
     return (
-      <div>
-        <h1>Welcome come mr {this.state.user.name}</h1>
-        <Link to="/"><button onClick={LogOut}>logout</button></Link>
+      <div className="container">
+        <Header /> { /* Header Components */ }
+        <main className="contents">
+          <div className="content">
+            <AsideNav /> { /* Left Navigation Components */ }
+            <div id="main-content">
+              <div>navigations</div>
+              <div id="requests">
+                <div id="req-header" className="list">
+                  <div className="date">Date</div>
+                  <div className="desc">Description</div>
+                  <div className="reqid">Req Id</div>
+                  <div className="status">Status</div>
+                </div>
+                <div id="req-content">
+                  <div className="reqs">
+                    <div className="date">10/20/2019</div>
+                    <div className="desc">My site is shutting down for the ...</div>
+                    <div className="reqid">405839</div>
+                    <div className="status">
+                      <div className="text">Pending</div>
+                      <div className="time">Still in review</div>
+                      <div className="options">
+                        <div className="edit">Edit</div>
+                        <div className="del">Delete</div>
+                      </div>
+                    </div>
+                  </div>
+                  <RequestList requests={this.state.requests} /> { /* requests Components */ }
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+        <footer>
+          <h2>Make sure you dont forget to register</h2>
+          <Link to="/"><button onClick={logOut}>logout</button></Link>
+        </footer>
       </div>
     );
   }
