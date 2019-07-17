bd6ee6a4102b9d64d12e32f798de8b51521e2b6f
Samuel Christopher <Ebusameric@gmail.com>
2019-07-17 23:49:04 +0100

    add font awesome icons

--- client/pages/components/Header.js
+++ client/pages/components/Header.js
@@ -1,23 +1,31 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { logOut } from '../services';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className="header">
       <div className="logo-section">
-        <div id="logo">Logo</div>
+        <div id="logo">
+          <i className="fab fa-affiliatetheme"></i>
+        </div>
         <h3>Maintenance Tracker</h3>
       </div>
       <div className="nav-section">
-        <div id="notification">
+        <div id="notification" className="header-nav">
+          <i className="far fa-bell"></i>
           <p>Notification</p>
         </div>
-        <div id="new-req">
-          <p>New</p>
+        <div id="new-req" onClick={props.makeNewReq} className="header-nav">
+          <i className="fas fa-plus"></i>
+          <p>New Request</p>
         </div>
-        <div id="profile">
-          <p>Profile</p>
-        </div>
+        <Link to="/">
+          <div id="logOut" className="header-nav" onClick={logOut}>
+            <i className="fas fa-sign-out-alt"></i>
+            <p>Logout</p>
+          </div>
+        </Link>
       </div>
     </header>
   );
