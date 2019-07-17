9b2cfec1cf6da9806d2f0cf457dfce2d2aee4b3d
Samuel Christopher <Ebusameric@gmail.com>
2019-07-17 23:47:50 +0100

    aside page navigation complete

--- client/pages/components/AsideNav.js
+++ client/pages/components/AsideNav.js
@@ -2,28 +2,84 @@ import React from 'react';
 
 class AsideNav extends React.Component {
   constructor(props) {
-      super(props);
+    super(props);
+    this.controller = this.controller.bind(this);
+  }
+
+  controller(liTag, iTag) {
+    return () => {
+      const initTag = document.querySelectorAll('#nav .fas')[0];
+      initTag.className = initTag.className.replace('fas ', 'far ');
+      iTag.className = iTag.className.replace('far ', 'fas ');
+
+      document.getElementById('active').id = '';
+      liTag.id = 'active';
+      this.props.filterRequests(liTag.className);
+    };
+  }
+
+  componentDidMount() {
+    const navItems = document.querySelectorAll('#aside-nav #nav ul li');
+    const icons = document.querySelectorAll('#aside-nav #nav ul li div i');
+    for (let i = 0; i < navItems.length; i++) {
+      navItems[i].onclick = this.controller(navItems[i], icons[i]);
+    }
   }
 
   render() {
     return (
       <div id="aside-nav">
-        <div id="user-prof">
-          <h3>Hi Samuel</h3>
-          <div id="user-img"></div>
-        </div>
         <div id="nav">
           <ul>
-            <li><span>All Requests</span> <span className="data">75</span></li>
-            <li>Pending Rquests</li>
-            <li>Approved Requests</li>
-            <li>Rejected Requests</li>
-            <li>Resolved Requests</li>
+            <li className='profile'>
+              <div className="icons">
+                <i className="far fa-user"></i>
+                <span>Profile</span>
+              </div>
+            </li>
+            <li id='active' className="requests">
+              <div className="icons">
+                <i className="fas fa-address-book"></i>
+                <span>All Requests</span>
+              </div>
+              <div className="data">{this.props.requestsL}</div>
+            </li>
+            <li className="pending">
+              <div className="icons">
+                <i className="far fa-hourglass"></i>
+                <span>Pending Requests</span>
+              </div>
+              <span className="data">{this.props.pendingL}</span>
+            </li>
+            <li className="approved">
+              <div className="icons">
+                <i className="far fa-thumbs-up"></i>
+                <span>Approved Requests</span>
+              </div>
+              <span className="data">{this.props.approvedL}</span>
+            </li>
+            <li className="rejected">
+              <div className="icons">
+                <i className="far fa-window-close"></i>
+                <span>Rejected Requests</span>
+              </div>
+              <span className="data">{this.props.rejectedL}</span>
+            </li>
+            <li className="resolved">
+              <div className="icons">
+                <i className="far fa-calendar-check"></i>
+                <span>Resolved Requests</span>
+              </div>
+              <span className="data">{this.props.resolvedL}</span>
+            </li>
+            <li className="message">
+              <div className="icons">
+                <i className="far fa-envelope-open"></i>
+                <span>Message Admin</span>
+              </div>
+              <span className="data"></span>
+            </li>
           </ul>
-        </div>
-        <div id="chat">
-          <h3>Chat Admin</h3>
-          <p>you can chat with our Admin, in case you’re having an issues.</p>
         </div>
       </div>
     );
