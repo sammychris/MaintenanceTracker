7c9fbe8f3e23a02b3b737ac1a86290f391b8c4d7
Samuel Christopher <Ebusameric@gmail.com>
2019-08-05 16:11:05 +0100

    refactor: code restructure

--- client/pages/components/RequestList.js
+++ client/pages/components/RequestList.js
@@ -1,60 +1,57 @@
 import React from 'react';
 
+const dateStyle = { fontSize: '12px' };
+
+const color = {
+  approved: { color: '#2D71CA', background: '#F2F3F8' },
+  resolved: { color: '#27873F' },
+  rejected: { color: 'red' },
+};
+
+const liColor = {
+  approved: { fontWeight: 'normal' },
+  rejected: { background: '#FAE5E4', fontWeight: 'normal' },
+  resolved: { background: '#e6ecf0', fontWeight: 'normal' },
+};
+
+const statusOption = {
+  pending: 'Still in review',
+  approved: '3days to resolve',
+  resolved: 'Finally resolved',
+  rejected: 'Failed',
+};
+
+
 const RequestList = (props) => {
-  const color = {
-    approved: { color: '#2D71CA', background: '#F2F3F8' },
-    resolved: { color: '#27873F' },
-    rejected: { color: 'red' },
-  };
+  const statusOp = ['rejected', 'pending', 'resolved'];
+  const {
+    requests, editReq, deleteReq, showRequest,
+  } = props;
 
-  const liColor = {
-    approved: { fontWeight: 'normal' },
-    rejected: { background: '#FAE5E4', fontWeight: 'normal' },
-    resolved: { background: '#e6ecf0', fontWeight: 'normal' },
-  };
+  return requests.map((item, index) => {
+    const {
+      status, date, description, reqId, _id, type,
+    } = item;
 
-  const statusOption = {
-    pending: 'Still in review',
-    approved: '3days to resolve',
-    resolved: 'Finally resolved',
-    rejected: 'Failed',
-  };
-
-  const Options = (v) => {
-    let Edit = '';
-    let Del = '';
-    if (v === 'pending') Edit = <div className="edit"><i className="far fa-edit"></i></div>;
-    if (v === 'rejected' || v === 'pending' || v === 'resolved') Del = <div className="del"><i className="far fa-trash-alt"></i></div>;
+    const dateStr = new Date(date).toDateString();
     return (
-      <div className="options">
-        { Edit }
-        { Del }
-      </div>
-    );
-  };
-
-  // const Status = (v) => {
-  //   if (v === 'approved') return <div> <i className="fas fa-thumbs-up"></i></div>;
-  //   if (v === 'resolved') return <i className="fas fa-calendar-check"></i>;
-  //   if (v === 'rejected') return <i className="fas fa-window-close"></i>;
-  //   return <span style={{ color: 'orange', fontSize: '20px', marginRight: '5px' }}><i className="far fa-hourglass"></i></span>;
-  // };
-
-  const dateStyle = { fontSize: '12px' };
-
-  return props.requests.map((item, index) => {
-    const date = new Date(item.date).toDateString();
-    return (
-      <div className="reqs" key={index} style={liColor[item.status]}>
-        <div className="date" style={dateStyle}>{ date }</div>
-        <div className="desc">{ item.description }</div>
-        <div className="reqid">{ item.reqId }</div>
+      <div className="reqs" key={ index } style={ liColor[status] } onClick={ showRequest} >
+        <div className="date" style={ dateStyle }>{ dateStr }</div>
+        <div className="desc">{ description }</div>
+        <div className="reqid">{ reqId }</div>
         <div className="statusOption">
           <div className="status">
-            <div className="text" style={ color[item.status] }>{ item.status}</div>
-            <div className="time">{ statusOption[item.status] }</div>
+            <div className="text" style={ color[status] }>{ status}</div>
+            <div className="time">{ statusOption[status] }</div>
           </div>
-          { Options(item.status) }
+          <div className="options">
+            { status === 'pending'
+              ? <div className="edit" onClick={editReq(_id, type, description)}><i className="far fa-edit"></i></div> : ''
+            }
+            { statusOp.includes(status)
+              ? <div className="del" onClick={deleteReq(_id)}><i className="far fa-trash-alt"></i></div> : ''
+            }
+          </div>
         </div>
       </div>
     );
